97341e4c6b2b88df26bea3456b47e53730b7b657
Denis Kuzmin <nirdest@gmail.com>
2023-11-13 22:53:14 +0600

    Update newstage.sh

--- kind/newstage.sh
+++ kind/newstage.sh
@@ -3,3 +3,13 @@ helm upgrade --install nginx-ingress --create-namespace --namespace nginx-ingres
 --set prometheus.service.create=true \
 --set controller.replicaCount=3 \
 --set controller.watchNamespace="default\,nginx-ingress\,elasticsearch"
+
+
+helm upgrade --install elasticsearch --create-namespace --namespace elasticsearch --version 7.17.3 \
+elastic/elasticsearch \
+--set secret.password="password"
+
+helm upgrade --install kibana --create-namespace --namespace elasticsearch --version 7.17.3 elastic/kibana \
+--set ingress.enabled=true \
+--set 'ingress.hosts[0].host=kibana.10.255.255.11.nip.io' \
+--set 'ingress.hosts[0].paths[0].path=\/'
